a3487bb7f26a9b5e68bf2f8f38c7d44f6b91b871
瞌睡虫 <lsl.yx@foxmail.com>
2017-06-23 22:35:07 +0800

    第38次提交,基本完成问题反馈页，关于app也，帮助页的功能

--- app/src/main/java/app/com/lsl/imagelabelerapp/lsl/activity/FeedBackActivity.java
+++ app/src/main/java/app/com/lsl/imagelabelerapp/lsl/activity/FeedBackActivity.java
@@ -4,7 +4,11 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import app.com.lsl.imagelabelerapp.R;
 import app.com.lsl.imagelabelerapp.lsl.activity.menu.TopMenuHeader;
@@ -14,11 +18,22 @@ import app.com.lsl.imagelabelerapp.lsl.activity.menu.TopMenuHeader;
  */
 
 public class FeedBackActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button but_commit;
+    private EditText et_tel;
+    private EditText et_content;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         initMenu();
+
+        et_content = (EditText) findViewById(R.id.et_problem_content);
+        et_tel = (EditText) findViewById(R.id.et_problem_tel);
+        but_commit = (Button) findViewById(R.id.but_problem_commit);
+        but_commit.setOnClickListener(this);
+
     }
 
     private void initMenu() {
@@ -41,6 +56,22 @@ public class FeedBackActivity extends AppCompatActivity implements View.OnClickL
                 break;
             case R.id.iv_icon:
                 finish();
+                break;
+            case R.id.but_problem_commit:
+                if (!TextUtils.isEmpty(et_content.getText().toString())) {
+                    // do something
+                    String pro_content = et_content.getText().toString().trim();
+                    String tel = et_tel.getText().toString().trim();
+                    try {
+                        Thread.sleep(1000);
+                        Toast.makeText(FeedBackActivity.this, "提交成功", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(FeedBackActivity.this, "提交失败，请填写问题或意见", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
